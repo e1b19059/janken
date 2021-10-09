85b6e940daad4afa5bf69e2ca233beb13adf7dd3
e1b19059 <e1b19059@oit.ac.jp>
2021-10-09 15:00:56 +0900

    名前入力による画面遷移を実装

---
+++ src/main/java/oit/is/z0316/kaizi/janken/model/Janken.java
@@ -0,0 +1,9 @@
+package oit.is.z0316.kaizi.janken.model;
+import java.util.ArrayList;
+
+/**
+ * Spring BootでModelとして扱われるクラス フレームワークがフィールドに値を代入したり，取得したりするため，getter/setterが必要
+ * これらをじゃんけん用に変更する
+ */
+public class Janken {
+}
